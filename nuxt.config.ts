27af377e68fe79e49cd4fcdca8540a7e8c91fee7
Ivailo1st <Ivailo2nd@gmail.com>
2024-12-07 21:44:01 +0100

    Adding nojekyll File

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
-    baseURL: '/nuxt-github-pages/',
-    buildAssetsDir: 'assets'
+    buildAssetsDir: 'assets',
+    
+
   },
   modules: [
     'nuxt-particles',
